943da37ab574d0e66455b569f2596f0dd94c5543
Bruno Levy <Bruno.Levy@inria.fr>
2020-06-01 20:57:59 +0200

    New test program for the led matrix.

--- NanoRV/FIRMWARE/EXAMPLES/test_led_matrix.s
+++ NanoRV/FIRMWARE/EXAMPLES/test_led_matrix.s
@@ -1,69 +1,111 @@
-# Testing the serial interface. Echoes typed characters,
-# and displays 4 LSBs using the LEDs.
-
 .section .text
 .globl _start
 .include "nanorv.s"
 
-MAX2719:sw a0,IO_LEDMTX_DATA(gp)
-waitMAX:lw t0,IO_LEDMTX_CNTL(gp)
-	bnez t0,waitMAX
+MAX2719: # a0: register  a1: value
+         slli t0, a0, 8
+	 or  t0, t0, a1
+         sw t0, IO_LEDMTX_DATA(gp)
+MAXwait: lw t0, IO_LEDMTX_CNTL(gp)
+         bnez t0, MAXwait
+	 ret
+
+MAX2719_init:
+	 add sp,sp,-4
+         sw ra, 0(sp)	
+         li a0, 0x09 # decode mode
+	 li a1, 0x00 
+	 call MAX2719
+	 li a0, 0x0a # intensity
+	 li a1, 0x0f
+	 call MAX2719
+	 li a0, 0x0b # scan limit
+	 li a1, 0x07
+	 call MAX2719
+	 li a0, 0x0c # shutdown
+	 li a1, 0x01
+	 call MAX2719
+	 li a0, 0x0f # display test
+	 li a1, 0x00
+	 call MAX2719
+ 	 lw ra, 0(sp)
+	 add sp,sp,4
+	 ret
+
+face1:
+        add sp,sp,-4
+        sw ra, 0(sp)	
+        li a0, 1
+	li a1, 0b00111100
+        call MAX2719
+	li a0, 2
+	li a1, 0b01000010
+	call MAX2719
+	li a0, 3
+	li a1, 0b10001001
+	call MAX2719
+	li a0, 4
+	li a1, 0b10100001
+	call MAX2719
+	li a0, 5
+	li a1, 0b10100001
+	call MAX2719
+	li a0, 6
+	li a1, 0b10001001
+	call MAX2719
+	li a0, 7
+	li a1, 0b01000010
+	call MAX2719
+        li a0, 8
+	li a1, 0b00111100
+        call MAX2719
+	lw ra, 0(sp)
+	add sp,sp,4
 	ret
-	
+
+face2:
+        add sp,sp,-4
+        sw ra, 0(sp)	
+        li a0, 1
+	li a1, 0b00111100
+        call MAX2719
+	li a0, 2
+	li a1, 0b01001010
+	call MAX2719
+	li a0, 3
+	li a1, 0b10011101
+	call MAX2719
+	li a0, 4
+	li a1, 0b10101001
+	call MAX2719
+	li a0, 5
+	li a1, 0b10101001
+	call MAX2719
+	li a0, 6
+	li a1, 0b10011101
+	call MAX2719
+	li a0, 7
+	li a1, 0b01001010
+	call MAX2719
+        li a0, 8
+	li a1, 0b00111100
+        call MAX2719
+	lw ra, 0(sp)
+	add sp,sp,4
+	ret
+
+
 _start:
-        li a0,0x0900  # decode mode
-	call MAX2719
-	li a0,0x0a0f  # intensity
-	call MAX2719
-	li a0,0x0b07  # scan limit
-	call MAX2719
-	li a0,0x0c01  # shutdown
-	call MAX2719
-	li a0,0x0f00  # display test
-	call MAX2719
+        call MAX2719_init
 
-animate:
-
-	li a0,0x0100  
-	call MAX2719	
-	li a0,0x02ff  
-	call MAX2719	
-	li a0,0x0300  
-	call MAX2719	
-	li a0,0x04ff  
-	call MAX2719	
-	li a0,0x0500  
-	call MAX2719	
-	li a0,0x06ff  
-	call MAX2719	
-	li a0,0x0700  
-	call MAX2719
-	li a0,0x08ff
-	call MAX2719	
+anim:   call face1
+        call wait
+        call wait	
+	call face2
+	call wait
+        call wait	
+	j anim
 	
-	call wait
-	call wait
 
-	li a0,0x01ff  
-	call MAX2719	
-	li a0,0x0200  
-	call MAX2719	
-	li a0,0x03ff  
-	call MAX2719	
-	li a0,0x0400  
-	call MAX2719	
-	li a0,0x05ff  
-	call MAX2719	
-	li a0,0x0600  
-	call MAX2719	
-	li a0,0x07ff  
-	call MAX2719
-	li a0,0x0800
-	call MAX2719	
-
-	call wait
-	call wait
-
-j animate
 
 
