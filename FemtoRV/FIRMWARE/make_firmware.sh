b1108bf4c82a4fda7cbea4681cb8ea5d8b316280
Bruno Levy <Bruno.Levy@inria.fr>
2020-11-28 22:42:34 +0100

    Cleaner makefiles.

--- FemtoRV/FIRMWARE/make_firmware.sh
+++ FemtoRV/FIRMWARE/make_firmware.sh
@@ -1,16 +1,14 @@
 
-#VERBOSE=1  #uncomment this line and comment the next one to debug library compilation
-VERBOSE="/dev/null"
 
 echo "============> Compiling libs"
-(cd LIBFEMTORV32; make clean libfemtorv32.a > $VERBOSE) # Compile hardware support lib
-(cd LIBFEMTOC;    make clean libfemtoc.a    > $VERBOSE) # Compile lib with printf() replacement function
+(cd LIBFEMTORV32; make clean all) # Compile hardware support lib
+(cd LIBFEMTOC;    make clean all) # Compile lib with printf() replacement function
 # Note: I 'make clean' each time, because there is no much to recompile (and dependencies
 # are not specified)...
 
-echo "============>" Making $EXE_BASENAME
 EXE_BASENAME=`basename $1 | sed -e 's|\.c$||' -e 's|\.S$||'`
 SOURCE_DIR=`dirname $1`
+echo "============>" Making $EXE_BASENAME
 (cd $SOURCE_DIR; make clean $EXE_BASENAME".hex" $EXE_BASENAME".exe")
 
 rm -f firmware.hex
