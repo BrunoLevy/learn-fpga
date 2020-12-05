422d05dda092964f51b591ba2ba9093b309b05c2
Bruno Levy <Bruno.Levy@inria.fr>
2020-12-06 00:26:46 +0100

    Working on tutorial terminal now in Makefile

--- FemtoRV/start_terminal.sh
+++
@@ -1,7 +0,0 @@
-# Start communication with the UART on femtosoc/femtorv32
-# Install python3-serial
-# Note: it may be called miniterm.py rather than miniterm
-miniterm --dtr=0 /dev/ttyUSB1 115200
-# or screen /dev/ttyUSB1 115200 (<ctrl> a \ to exit)
-
-
