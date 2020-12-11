1d513eb08323039ec2e334a99f013401df61f881
Bruno Levy <Bruno.Levy@inria.fr>
2020-12-11 12:27:52 +0100

    More tests with the mini_decoder.v

--- FemtoRV/RTL/mini_decoder.v
+++ FemtoRV/RTL/mini_decoder.v
@@ -1,13 +1,16 @@
 /********************* Instruction decoder *******************************/
 /* A drop-in replacement of the instruction decoder, meant to further    */
-/* reduce LUT count by not checking for errors (but no success for now)  */
+/* reduce LUT count by not checking for errors.                          */
+/* Optimized by @mecrisp                                                 */
+/* in femtorv32.v, replace `include "decoder.v"                          */
+/*                 with    `include "mini_decoder.v"                     */
 
 module NrvDecoder(
     input wire [31:0] instr,
     output wire [4:0] writeBackRegId,
     output reg 	      writeBackEn,
     output reg [3:0]  writeBackSel, // 0001: ALU  0010: PC+4  0100: RAM 1000: counters
-		                    // (could use 2 wires instead, but using 4 wires (1-hot encoding) 
+		                    // (could use 2 wires instead, but using 4 wires (1-hot encoding)
 		                    //  reduces both LUT count and critical path in the end !)
     output wire [4:0] inRegId1,
     output wire [4:0] inRegId2,
@@ -27,27 +30,27 @@ module NrvDecoder(
 );
 
    assign error = 1'b0; // We do not check for errors in the MiniDecoder.
-   assign aluM  = 1'b0; // MiniDecoder only works for RV32I 
-   
-    
+   assign aluM  = 1'b0; // MiniDecoder only works for RV32I
+
+
    reg inRegId1Sel; // 0: force inRegId1 to zero 1: use inRegId1 instr field
-   
+
    assign writeBackRegId = instr[11:7];
    assign inRegId1       = instr[19:15] & {5{inRegId1Sel}}; // Internal sig InRegId1Sel used to force zero in reg1
    assign inRegId2       = instr[24:20];             // (because I'm making maximum reuse of the adder of the ALU)
-   assign aluOp          = instr[14:12];  
+   assign aluOp          = instr[14:12];
 
    wire [31:0] Iimm = {{21{instr[31]}}, instr[30:20]};
    wire [31:0] Simm = {{21{instr[31]}}, instr[30:25], instr[11:7]};
    wire [31:0] Bimm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-   wire [31:0] Jimm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};   
+   wire [31:0] Jimm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
    wire [31:0] Uimm = {instr[31], instr[30:12], {12{1'b0}}};
 
    // The rest of instruction decoding, for the following signals:
    // writeBackEn
    // writeBackSel   0001: ALU  0010: PC+4 0100: RAM 1000: counters
    // inRegId1Sel    0: zero   1: regId
-   // aluInSel1      0: reg    1: PC 
+   // aluInSel1      0: reg    1: PC
    // aluInSel2      0: reg    1: imm
    // aluQual        +/- SRLI/SRAI
    // aluM           1 if instr is RV32M
@@ -60,7 +63,7 @@ module NrvDecoder(
    //  - For ALU ops with immediates, aluQual is 0, except
    //    for shifts (then it is instr[30]).
    wire aluOpIsShift = (aluOp == 3'b001) || (aluOp == 3'b101);
-   
+
    always @(*) begin
 
        nextPCSel = 3'b001; // default: PC <- PC+4
@@ -71,59 +74,59 @@ module NrvDecoder(
        needWaitALU = 1'b0;
 
        (* parallel_case, full_case *)
-       case(instr[6:2])
-	   5'b01101: begin // LUI
+       casez(instr[6:2])
+	   5'b011?1: begin // LUI
 	      writeBackEn  = 1'b1;    // enable write back
 	      writeBackSel = 4'b0001; // write back source = ALU
 	      inRegId1Sel = 1'b0;     // reg 1 Id = 0
-	      aluInSel1 = 1'b0;       // ALU source 1 = reg	      
+	      aluInSel1 = 1'b0;       // ALU source 1 = reg
 	      aluInSel2 = 1'b1;       // ALU source 2 = imm
 	      aluSel = 1'b0;          // ALU op = ADD
 	      imm = Uimm;             // imm format = U
 	   end
-	 
-	   5'b00101: begin // AUIPC
+
+	   5'b001?1: begin // AUIPC
 	      writeBackEn  = 1'b1;    // enable write back
 	      writeBackSel = 4'b0001; // write back source = ALU
-	      inRegId1Sel = 1'bx;     // reg 1 Id : don't care (we use PC)	      
-	      aluInSel1 = 1'b1;       // ALU source 1 = PC	      
+	      inRegId1Sel = 1'bx;     // reg 1 Id : don't care (we use PC)
+	      aluInSel1 = 1'b1;       // ALU source 1 = PC
 	      aluInSel2 = 1'b1;       // ALU source 2 = imm
 	      aluSel = 1'b0;          // ALU op = ADD
 	      imm = Uimm;             // imm format = U
 	   end
-	 
+
 	   5'b11011: begin // JAL
 	      writeBackEn  = 1'b1;    // enable write back
 	      writeBackSel = 4'b0010; // write back source = PC+4
-	      inRegId1Sel = 1'bx;     // reg 1 Id : don't care (we use PC)	      	      
-	      aluInSel1 = 1'b1;       // ALU source 1 = PC	      
+	      inRegId1Sel = 1'bx;     // reg 1 Id : don't care (we use PC)
+	      aluInSel1 = 1'b1;       // ALU source 1 = PC
 	      aluInSel2 = 1'b1;       // ALU source 2 = imm
 	      aluSel = 1'b0;          // ALU op = ADD
-	      nextPCSel = 3'b010;     // PC <- ALU	      
+	      nextPCSel = 3'b010;     // PC <- ALU
 	      imm = Jimm;             // imm format = J
 	   end
-	 
+
 	   5'b11001: begin // JALR
 	      writeBackEn  = 1'b1;    // enable write back
 	      writeBackSel = 4'b0010; // write back source = PC+4
-	      aluInSel1 = 1'b0;       // ALU source 1 = reg	      
+	      aluInSel1 = 1'b0;       // ALU source 1 = reg
 	      aluInSel2 = 1'b1;       // ALU source 2 = imm
 	      aluSel = 1'b0;          // ALU op = ADD
-	      nextPCSel = 3'b010;     // PC <- ALU	      
+	      nextPCSel = 3'b010;     // PC <- ALU
 	      imm = Iimm;             // imm format = I
 	   end
-	 
-	   5'b11000: begin // Branch
+
+	   5'b110?0: begin // Branch
 	      writeBackEn = 1'b0;     // disable write back
 	      writeBackSel = 4'bxxxx; // write back source = don't care
 	      aluInSel1 = 1'b1;       // ALU source 1 : PC
 	      aluInSel2 = 1'b1;       // ALU source 2 : imm
 	      aluSel = 1'b0;          // ALU op = ADD
-	      nextPCSel = 3'b100;     // PC <- pred ? ALU : PC+4	       
+	      nextPCSel = 3'b100;     // PC <- pred ? ALU : PC+4
 	      imm = Bimm;             // imm format = B
 	   end
-	   
-	   5'b00100: begin // ALU operation: Register,Immediate
+
+	   5'b001?0: begin // ALU operation: Register,Immediate
 	      writeBackEn = 1'b1;     // enable write back
 	      writeBackSel = 4'b0001; // write back source = ALU
 	      aluInSel1 = 1'b0;       // ALU source 1 : reg
@@ -134,8 +137,8 @@ module NrvDecoder(
 	      aluSel = 1'b1;         // ALU op : from instr
 	      imm = Iimm;            // imm format = I
 	   end
-	   
-	   5'b01100: begin // ALU operation: Register,Register
+
+	   5'b011?0: begin // ALU operation: Register,Register
 	      writeBackEn = 1'b1;     // enable write back
 	      writeBackSel = 4'b0001; // write back source = ALU
 	      aluInSel1 = 1'b0;       // ALU source 1 : reg
@@ -145,8 +148,8 @@ module NrvDecoder(
 	      needWaitALU = aluOpIsShift;
 	      imm = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx; // don't care
 	   end
-	   
-           5'b00000: begin // Load
+
+	   5'b000?0: begin // Load
 	      writeBackEn = 1'b1;     // enable write back
 	      writeBackSel = 4'b0100; // write back source = RAM
 	      aluInSel1 = 1'b0;       // ALU source 1 = reg
@@ -155,8 +158,8 @@ module NrvDecoder(
 	      imm = Iimm;             // imm format = I
 	      isLoad = 1'b1;
 	   end
-	 
-           5'b01000: begin // Store
+
+	   5'b010?0: begin // Store
 	      writeBackEn = 1'b0;     // disable write back
 	      writeBackSel = 4'bxxxx; // write back sel = don't care
 	      aluInSel1 = 1'b0;       // ALU source 1 = reg
@@ -165,18 +168,19 @@ module NrvDecoder(
 	      imm = Simm;             // imm format = S
 	      isStore = 1'b1;
 	   end
-	    
-           default: begin
+
+	   default: begin
 	      writeBackEn = 1'b0;
-	      writeBackSel = 4'bxxxx;   
-	      inRegId1Sel = 1'bx; 
-	      aluInSel1 = 1'bx;      
-	      aluInSel2 = 1'bx;      
-	      aluSel = 1'bx;      
-	      nextPCSel = 3'bxxx;  
+	      writeBackSel = 4'bxxxx;
+	      inRegId1Sel = 1'bx;
+	      aluInSel1 = 1'bx;
+	      aluInSel2 = 1'bx;
+	      aluSel = 1'bx;
+	      nextPCSel = 3'bxxx;
 	      imm = 32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
 	   end
        endcase
    end
 
 endmodule
+
