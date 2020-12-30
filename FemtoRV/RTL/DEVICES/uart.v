140122cb7dcd524f1a9e5c4174247d6d0403374d
Bruno Levy <Bruno.Levy@inria.fr>
2020-12-31 00:27:18 +0100

    Added Claire Wolf's UART

--- FemtoRV/RTL/DEVICES/uart.v
+++ FemtoRV/RTL/DEVICES/uart.v
@@ -4,6 +4,52 @@
 //
 // This file: driver for UART (serial over USB)
 
+//`define USE_CLAIRE_UART
+
+`ifdef USE_CLAIRE_UART
+
+`define cfg_divider (`NRV_FREQ * 1000 * 1000 / 115200)
+`include "uart_picosoc.v"
+
+module UART(
+    input wire 	       clk,   // system clock
+    input wire 	       rstrb, // read strobe		
+    input wire 	       wstrb, // write strobe
+    input wire 	       sel,   // select data (rw) 	       
+    input wire [31:0]  wdata, // data to be written
+    output wire [31:0] rdata, // data read
+
+    input wire 	       RXD, // UART pins
+    output wire        TXD	    
+);
+
+wire [7:0] rx_data;
+wire [7:0] tx_data;
+wire serial_tx_busy;
+wire serial_valid;
+
+buart #(
+  .FREQ_MHZ(`NRV_FREQ),
+  .BAUDS(115200)		  
+) the_buart (
+   .clk(clk),
+   .resetq(1'b1),
+   .tx(TXD),
+   .rx(RXD),
+   .tx_data(wdata[7:0]),
+   .rx_data(rx_data),
+   .busy(serial_tx_busy),
+   .valid(serial_valid),
+   .wr(sel && wstrb),
+   .rd(sel && rstrb)
+);
+
+assign rdata = sel ? {22'b0, serial_tx_busy, serial_valid, rx_data} : 32'b0;   
+   
+endmodule
+
+`else
+
 `define CLKFREQ   (`NRV_FREQ*1000000) // Used by uart_impl.v (clk freq in Hz, NRV_FREQ is in MHz).
 `include "uart_impl.v"                // (uart_impl is borrowed from J1 swapforth)
 
@@ -19,7 +65,7 @@ module UART(
     output wire        TXD	    
 );
 
-/******************** UART Receiver ****************************/
+//******************** UART Receiver ***************************
 
    reg serial_valid_latched = 1'b0;
    wire serial_valid;
@@ -44,7 +90,7 @@ module UART(
       end
    end
 
-/******************** UART transmitter ****************************/
+//******************** UART transmitter ***************************
 
    wire       serial_tx_busy;
    wire       serial_tx_wr;
@@ -61,3 +107,6 @@ module UART(
    assign serial_tx_wr = wstrb && sel;
 
 endmodule
+
+`endif
+
---
+++ FemtoRV/RTL/DEVICES/uart_picosoc.v
@@ -0,0 +1,133 @@
+/*
+ *  PicoSoC - A simple example SoC using PicoRV32
+ *
+ *  Copyright (C) 2017  Clifford Wolf <clifford@clifford.at>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+ // October 2019, Matthias Koch: Renamed wires
+ // December 2020, Bruno Levy: parameterization with freq and bauds
+
+module buart #(
+  parameter FREQ_MHZ = 60,
+  parameter BAUDS    = 115200	       
+) (
+    input clk,
+    input resetq,
+
+    output tx,
+    input  rx,
+
+    input  wr,
+    input  rd,
+    input  [7:0] tx_data,
+    output [7:0] rx_data,
+
+    output busy,
+    output valid
+);
+
+    parameter divider = FREQ_MHZ * 1000000 / BAUDS;
+   
+    reg [3:0] recv_state;
+    reg [$clog2(divider)-1:0] recv_divcnt;   // Counts to divider. Reserve enough bytes !
+    reg [7:0] recv_pattern;
+    reg [7:0] recv_buf_data;
+    reg recv_buf_valid;
+
+    reg [9:0] send_pattern;
+    reg [3:0] send_bitcnt;
+    reg [$clog2(divider)-1:0] send_divcnt;   // Counts to divider. Reserve enough bytes !
+    reg send_dummy;
+
+    assign rx_data = recv_buf_data;
+    assign valid = recv_buf_valid;
+    assign busy = (send_bitcnt || send_dummy);
+
+    always @(posedge clk) begin
+        if (!resetq) begin
+
+            recv_state <= 0;
+            recv_divcnt <= 0;
+            recv_pattern <= 0;
+            recv_buf_data <= 0;
+            recv_buf_valid <= 0;
+
+        end else begin
+            recv_divcnt <= recv_divcnt + 1;
+
+            if (rd) recv_buf_valid <= 0;
+
+            case (recv_state)
+                0: begin
+                    if (!rx)
+                        recv_state <= 1;
+                    recv_divcnt <= 0;
+                end
+                1: begin
+                    if (2*recv_divcnt > divider) begin
+                        recv_state <= 2;
+                        recv_divcnt <= 0;
+                    end
+                end
+                10: begin
+                    if (recv_divcnt > divider) begin
+                        recv_buf_data <= recv_pattern;
+                        recv_buf_valid <= 1;
+                        recv_state <= 0;
+                    end
+                end
+                default: begin
+                   if (recv_divcnt > divider) begin
+                        recv_pattern <= {rx, recv_pattern[7:1]};
+                        recv_state <= recv_state + 1;
+                        recv_divcnt <= 0;
+                    end
+                end
+            endcase
+        end
+    end
+
+    assign tx = send_pattern[0];
+
+    always @(posedge clk) begin
+        send_divcnt <= send_divcnt + 1;
+        if (!resetq) begin
+            send_pattern <= ~0;
+            send_bitcnt <= 0;
+            send_divcnt <= 0;
+            send_dummy <= 1;
+        end else begin
+            if (send_dummy && !send_bitcnt) begin
+                send_pattern <= ~0;
+                send_bitcnt <= 15;
+                send_divcnt <= 0;
+                send_dummy <= 0;
+            end else
+            if (wr && !send_bitcnt) begin
+                send_pattern <= {1'b1, tx_data[7:0], 1'b0};
+                send_bitcnt <= 10;
+                send_divcnt <= 0;
+            end else
+            if (send_divcnt > divider && send_bitcnt) begin
+                send_pattern <= {1'b1, send_pattern[9:1]};
+                send_bitcnt <= send_bitcnt - 1;
+                send_divcnt <= 0;
+            end
+        end
+    end
+endmodule
+
+
