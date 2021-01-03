1e77672988602ef60209447ec220f8f863c5588b
Bruno Levy <Bruno.Levy@inria.fr>
2021-01-03 16:12:30 +0100

    Restored default config for IceStick

--- FemtoRV/RTL/femtosoc_config.v
+++ FemtoRV/RTL/femtosoc_config.v
@@ -3,24 +3,24 @@
 /************************* Devices **********************************************************************************/
 
 `define NRV_IO_LEDS        // Mapped IO, LEDs D1,D2,D3,D4 (D5 is used to display errors)
-//`define NRV_IO_UART        // Mapped IO, virtual UART (USB)
-`define NRV_IO_SSD1351     // Mapped IO, 128x128x64K OLed screen
+`define NRV_IO_UART        // Mapped IO, virtual UART (USB)
+//`define NRV_IO_SSD1351     // Mapped IO, 128x128x64K OLed screen
 //`define NRV_IO_MAX7219   // Mapped IO, 8x8 led matrix
 //`define NRV_IO_SPI_FLASH // Mapped IO, SPI flash  
-`define NRV_IO_SDCARD      // Mapped IO, SPI SDCARD
-`define NRV_IO_BUTTONS     // Mapped IO, buttons
+//`define NRV_IO_SDCARD      // Mapped IO, SPI SDCARD
+//`define NRV_IO_BUTTONS     // Mapped IO, buttons
 //`define NRV_MAPPED_SPI_FLASH // SPI flash mapped in address space. Use with MINIRV32 to run code from SPI flash.
-`define NRV_IO_FGA // Femto Graphic Adapter (ULX3S only)
+//`define NRV_IO_FGA // Femto Graphic Adapter (ULX3S only)
 
 /************************* Frequency ********************************************************************************/
 
-`define NRV_FREQ 80      // Frequency in MHz. Recomm: 50 MHz (FOMU: 16MHz) Overclocking: 80-100 MHz (HX1K, ECP5)
+`define NRV_FREQ 50      // Frequency in MHz. Recomm: 50 MHz (FOMU: 16MHz) Overclocking: 80-100 MHz (HX1K, ECP5)
 
 /************************* RAM (in bytes, needs to be a multiple of 4)***********************************************/
 
 //`define NRV_RAM 393216 // bigger config for ULX3S
-`define NRV_RAM 262144 // default for ULX3S
-//`define NRV_RAM 6144     // default for ICESTICK (cannot do more !)
+//`define NRV_RAM 262144 // default for ULX3S
+`define NRV_RAM 6144     // default for ICESTICK (cannot do more !)
 //`define NRV_RAM 1024   // small ICESTICK config (to further save LUTs if need be)
 
 /************************* Processor configuration ******************************************************************/
@@ -28,12 +28,12 @@
 //`define NRV_MINIRV32 // Mini config, can execute code stored in SPI flash from 1Mb offset (mapped to address 0x800000)
 
 `ifndef NRV_MINIRV32 // The options below are not supported by minifemtorv32
-`define NRV_CSR         // Uncomment if using something below (counters,...)
-`define NRV_COUNTERS    // Uncomment for instr and cycle counters (won't fit on the ICEStick)
-`define NRV_COUNTERS_64 // ... and uncomment this one as well if you want 64-bit counters
-`define NRV_RV32M       // Uncomment for hardware mul and div support (RV32M instructions). Not supported on IceStick !
+//`define NRV_CSR         // Uncomment if using something below (counters,...)
+//`define NRV_COUNTERS    // Uncomment for instr and cycle counters (won't fit on the ICEStick)
+//`define NRV_COUNTERS_64 // ... and uncomment this one as well if you want 64-bit counters
+//`define NRV_RV32M       // Uncomment for hardware mul and div support (RV32M instructions). Not supported on IceStick !
 `define NRV_TWOSTAGE_SHIFTER // if not RV32M, comment-out if running out of LUTs (at the expense of slower shifts)
-`define NRV_LATCH_ALU // Uncomment to latch all ALU ops (reduces critical path)
+//`define NRV_LATCH_ALU // Uncomment to latch all ALU ops (reduces critical path)
 `endif
 
 /************************* Advanced processor configuration *********************************************************/
