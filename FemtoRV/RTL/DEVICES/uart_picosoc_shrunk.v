7e3abbc3d31dda03faafc86ea2f3617090e376b6
Bruno Levy <Bruno.Levy@inria.fr>
2021-04-02 20:35:58 +0200

    Included Matthias Koch's update for the UART (full-duplex, more legible, a bit more compact)

--- FemtoRV/RTL/DEVICES/uart_picosoc_shrunk.v
+++ FemtoRV/RTL/DEVICES/uart_picosoc_shrunk.v
@@ -21,10 +21,10 @@
  // December 2020, Bruno Levy: parameterization with freq and bauds
  //                            Factorized recv_divcnt and send_divcnt
  //                            Additional LUT golfing tricks
- 
+
 module buart #(
-  parameter FREQ_MHZ = 60,
-  parameter BAUDS    = 115200	       
+  parameter FREQ_MHZ = 12,
+  parameter BAUDS    = 115200
 ) (
     input clk,
     input resetq,
@@ -41,95 +41,83 @@ module buart #(
     output valid
 );
 
-   /************** Baud frequency generator *******************/
-   
-    // Generates the bauds clock, and the 
-    // half-cycle for the receiver.
+   /************** Baud frequency constants ******************/
+
     parameter divider = FREQ_MHZ * 1000000 / BAUDS;
     parameter divwidth = $clog2(divider);
-   
-    /* verilator lint_off WIDTH */
-    parameter [divwidth:0] baud_init = divider;
-    parameter [divwidth:0] half_baud_init = divider/2+1; 
-    /* verilator lint_on WIDTH */
-   
-    // Trick from Olof Kindgren: use n+1 bit and decrement instead of
-    // incrementing, and test the sign bit.
-    reg [divwidth:0] divcnt; 
-    wire baud_clk  = divcnt[divwidth];
-    always @(posedge clk) begin
-       if((recv_state == 0) && !rx) begin
-           // half period for the first received bit
-          divcnt <= half_baud_init;
-       end else if(  
-           (wr && (send_bitcnt == 0)) || baud_clk 
-       ) begin
-          divcnt <= baud_init;
-       end else begin
-	  divcnt <= divcnt - 1;
-       end
-    end   
+
+    parameter baud_init = divider;
+    parameter half_baud_init = divider/2+1;
 
    /************* Receiver ***********************************/
-   
-    reg [3:0] recv_state;
-    reg [7:0] recv_pattern;
+
+    // Trick from Olof Kindgren: use n+1 bit and decrement instead of
+    // incrementing, and test the sign bit.
+
+    reg [divwidth:0] recv_divcnt;
+    wire recv_baud_clk = recv_divcnt[divwidth];
+
+    reg recv_state;
+    reg [8:0] recv_pattern;
     reg [7:0] recv_buf_data;
     reg recv_buf_valid;
 
     assign rx_data = recv_buf_data;
     assign valid = recv_buf_valid;
 
+
     always @(posedge clk) begin
 
-       if (rd) begin
-	  recv_buf_valid <= 0;
-       end
+       if (rd) recv_buf_valid <= 0;
 
        case (recv_state)
+
          0: begin
-            if (!rx) begin
-               recv_state <= 1;
-	    end
-         end
+               if (!rx) begin
+                 recv_state <= 1;
+		 /* verilator lint_off WIDTH */
+                 recv_divcnt <= half_baud_init;
+		 /* verilator lint_on WIDTH */
+               end
+               recv_pattern <= 0;
+            end
+
          1: begin
-	    // This one is triggered after a half-period
-	    // (state 0 initializes the counter with divider/2+1)
-            if (baud_clk) begin 
-               recv_state <= 2;
+               if (recv_baud_clk) begin
+
+                 // Inverted start bit shifted through the whole register ?
+                 if (recv_pattern[0]) begin
+                   recv_buf_data  <= ~recv_pattern[8:1];
+                   recv_buf_valid <= 1;
+                   recv_state <= 0;
+                 end else begin
+                   recv_pattern <= {~rx, recv_pattern[8:1]};
+		   /* verilator lint_off WIDTH */		    
+                   recv_divcnt <= baud_init;
+		   /* verilator lint_on WIDTH */		    		    
+                 end
+               end else recv_divcnt <= recv_divcnt - 1;
             end
-         end
-         10: begin
-            if (baud_clk) begin
-               recv_buf_data <= recv_pattern;
-               recv_buf_valid <= 1;
-               recv_state <= 0;
-            end
-         end
-         default: begin
-            if (baud_clk) begin
-               recv_pattern <= {rx, recv_pattern[7:1]};
-               recv_state <= recv_state + 1;
-            end
-         end
+
        endcase
     end
 
    /************* Transmitter ******************************/
-   
+
+    reg [divwidth:0] send_divcnt;
+    wire send_baud_clk  = send_divcnt[divwidth];
+
     reg [9:0] send_pattern = 1;
-    reg [3:0] send_bitcnt;
-    assign busy = |send_bitcnt;
     assign tx = send_pattern[0];
+    assign busy = |send_pattern[9:1];
 
     always @(posedge clk) begin
-       if (wr && !busy) begin
-          send_pattern <= {1'b1, tx_data[7:0], 1'b0};
-          send_bitcnt <= 10;
-       end else if (baud_clk && busy) begin
-          send_pattern <= {1'b1, send_pattern[9:1]};
-          send_bitcnt <= send_bitcnt - 1;
-       end
+       if (wr) send_pattern <= {1'b1, tx_data[7:0], 1'b0};
+       else if (send_baud_clk & busy) send_pattern <= send_pattern >> 1;
+       /* verilator lint_off WIDTH */		    
+       if (wr | send_baud_clk) send_divcnt <= baud_init;
+                          else send_divcnt <= send_divcnt - 1;
+       /* verilator lint_on WIDTH */		           
     end
 
 endmodule
