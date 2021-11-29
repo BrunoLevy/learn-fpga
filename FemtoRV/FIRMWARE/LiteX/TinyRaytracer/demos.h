0255d2cbadc54ed31fae5358bb827c19f33d41cc
Bruno Levy <Bruno.Levy@inria.fr>
2021-11-29 16:56:56 +0100

    Cleanup

--- FemtoRV/FIRMWARE/LiteX/TinyRaytracer/demos.h
+++ FemtoRV/FIRMWARE/LiteX/TinyRaytracer/demos.h
@@ -1,7 +1,7 @@
 
 /**
  * \brief Computes a simple raytracing image.
- * \param[in] tty_output if set, displays the
+ * \param[in] tty_output if non-zero, displays the
  *  scene on the terminal using color ansi 
  *  sequences. Deactivate it for benchmarking.
  */
