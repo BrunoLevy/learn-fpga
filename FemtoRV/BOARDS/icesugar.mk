5ba5bbf2c5a85372042c1cb91ffe06734729aaef
Paolo Pisati <p.pisati@gmail.com>
2021-12-23 15:44:10 +0100

    boards: icesugar: update icesugar.mk following icebreaker.mk
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

--- FemtoRV/BOARDS/icesugar.mk
+++ FemtoRV/BOARDS/icesugar.mk
@@ -1,13 +1,12 @@
-YOSYS_ICESUGAR_OPT=-DICE_SUGAR -q -p "synth_ice40 -relut -top $(PROJECTNAME) -json $(PROJECTNAME).json"
+YOSYS_ICESUGAR_OPT=-DICE_SUGAR -q -p "synth_ice40 -abc9 -device u -dsp -top $(PROJECTNAME) -json $(PROJECTNAME).json"
 NEXTPNR_ICESUGAR_OPT=--force --json $(PROJECTNAME).json --pcf BOARDS/icesugar.pcf --asc $(PROJECTNAME).asc \
-                       --freq 12 --up5k --package sg48
+                       --freq 12 --up5k --package sg48 --opt-timing
 
 #######################################################################################################################
 
 ICESUGAR: ICESUGAR.firmware_config ICESUGAR.synth ICESUGAR.prog
 
 ICESUGAR.synth:
-	TOOLS/make_config.sh -DICE_SUGAR
 	yosys $(YOSYS_ICESUGAR_OPT) $(VERILOGS)
 	nextpnr-ice40 $(NEXTPNR_ICESUGAR_OPT)
 	icetime -p BOARDS/icesugar.pcf -P sg48 -r $(PROJECTNAME).timings -d up5k -t $(PROJECTNAME).asc
