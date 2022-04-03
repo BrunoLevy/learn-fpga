5a2206b97f897ca96bbc0c205e28fddb28277fd1
Bruno Levy <Bruno.Levy@inria.fr>
2022-04-03 21:11:36 +0200

    Improved software examples and C examples.

--- FemtoRV/TUTORIALS/FROM_BLINKER_TO_RISCV/terminal.sh
+++ FemtoRV/TUTORIALS/FROM_BLINKER_TO_RISCV/terminal.sh
@@ -1,13 +1,11 @@
 DEVICE=/dev/ttyUSB1   # replace by the terminal used by your device 
 BAUDS=115200
 
-
-
 # MINITERM  exit: <ctrl> ]     package: sudo apt-get install python3-serial
-#miniterm --dtr=0 $DEVICE $BAUDS
+miniterm --dtr=0 $DEVICE $BAUDS
 
 # SCREEN    exit: <ctrl> a \   package: sudo apt-get install screen
-screen $DEVICE $BAUDS
+#screen $DEVICE $BAUDS
 
 # PICOCOM exit: <ctrl> a \   package: sudo apt-get install picocom
 #picocom -b $BAUDS $DEVICE --imap lfcrlf,crcrlf --omap delbs,crlf --send-cmd "ascii-xfr -s -l 30 -n"
