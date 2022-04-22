1edf1133fefe1668367e36a22061053e088d6bf4
Bruno Levy <Bruno.Levy@inria.fr>
2022-04-22 16:03:52 +0200

    Removed erroneously added files.

--- FemtoRV/TUTORIALS/FROM_BLINKER_TO_RISCV/FIRMWARE/hello.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void main() {
-   printf("hello, world\n");
-}
--- FemtoRV/TUTORIALS/FROM_BLINKER_TO_RISCV/FIRMWARE/mandel_C.s
+++
@@ -1,5 +0,0 @@
-	.file	"mandel_C.c"
-	.option nopic
-	.attribute arch, "rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0"
-	.attribute unaligned_access, 0
-	.attribute stack_align, 16
