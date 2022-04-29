cb59af17890c162e24cdbf0b9f230b8a2a9c299d
abderraouf-adjal <adjal.arf@gmail.com>
2022-04-29 20:55:27 +0100

    Update icesugar_nano.mk

--- FemtoRV/BOARDS/icesugar_nano.mk
+++ FemtoRV/BOARDS/icesugar_nano.mk
@@ -6,14 +6,13 @@ NEXTPNR_ICESUGAR_NANO_OPT=--force --json $(PROJECTNAME).json --pcf BOARDS/icesug
 
 ICESUGAR_NANO: ICESUGAR_NANO.firmware_config ICESUGAR_NANO.synth ICESUGAR_NANO.prog 
 
-ICESUGAR_NANO.synth: FIRMWARE/firmware.hex 
-	TOOLS/make_config.sh -DICE_SUGAR_NANO
+ICESUGAR_NANO.synth:
 	yosys $(YOSYS_ICESUGAR_NANO_OPT) $(VERILOGS)
 	nextpnr-ice40 $(NEXTPNR_ICESUGAR_NANO_OPT)
 	icetime -p BOARDS/icesugar_nano.pcf -P cm36 -r $(PROJECTNAME).timings -d lp1k -t $(PROJECTNAME).asc
 	icepack -s $(PROJECTNAME).asc $(PROJECTNAME).bin
 
-ICESUGAR_NANO.show: FIRMWARE/firmware.hex 
+ICESUGAR_NANO.show:
 	yosys $(YOSYS_ICESUGAR_NANO_OPT) $(VERILOGS)
 	nextpnr-ice40 $(NEXTPNR_ICESUGAR_NANO_OPT) --gui
 
@@ -23,3 +22,4 @@ ICESUGAR_NANO.prog:
 ICESUGAR_NANO.firmware_config:
 	BOARD=icesugar_nano TOOLS/make_config.sh -DICE_SUGAR_NANO
 	(cd FIRMWARE; make libs)
+
