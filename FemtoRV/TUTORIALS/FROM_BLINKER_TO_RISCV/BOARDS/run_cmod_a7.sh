4a086539222b1c1f067dd66a0ad7d8ce488fb1c8
Bastian Löher <b.loeher@gsi.de>
2023-04-30 22:35:12 +0200

    Re-enable yosys step.

--- FemtoRV/TUTORIALS/FROM_BLINKER_TO_RISCV/BOARDS/run_cmod_a7.sh
+++ FemtoRV/TUTORIALS/FROM_BLINKER_TO_RISCV/BOARDS/run_cmod_a7.sh
@@ -8,7 +8,7 @@ BOARD_FREQ=100
 CPU_FREQ=100
 
 set -ex
-# yosys -DCMODA7 -DBOARD_FREQ=$BOARD_FREQ -DCPU_FREQ=$CPU_FREQ -p "scratchpad -set xilinx_dsp.multonly 1" -p "synth_xilinx -nowidelut -flatten -abc9 -arch xc7 -top SOC; write_json ${PROJECT_NAME}.json" ${VERILOGS}
+yosys -DCMODA7 -DBOARD_FREQ=$BOARD_FREQ -DCPU_FREQ=$CPU_FREQ -p "scratchpad -set xilinx_dsp.multonly 1" -p "synth_xilinx -nowidelut -flatten -abc9 -arch xc7 -top SOC; write_json ${PROJECT_NAME}.json" ${VERILOGS}
 nextpnr-xilinx --chipdb ${CHIPDB_DIR}/xc7a35tcpg236-1.bin --xdc BOARDS/cmod_a7.xdc --json ${PROJECT_NAME}.json --write ${PROJECT_NAME}_routed.json --fasm ${PROJECT_NAME}.fasm
 fasm2frames --part ${PART} --db-root ${DB_DIR}/artix7 ${PROJECT_NAME}.fasm > ${PROJECT_NAME}.frames
 xc7frames2bit --part_file ${DB_DIR}/artix7/${PART}/part.yaml --part_name ${PART} --frm_file ${PROJECT_NAME}.frames --output_file ${PROJECT_NAME}.bit
