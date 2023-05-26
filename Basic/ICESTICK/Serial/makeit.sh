32730a2cc31259eecd1d72709997c13f0efc560d
Maxin John <maxin.john@gmail.com>
2023-05-26 20:00:28 +0300

    ICESTICK: Serial: minor fix in makeit.sh
    Correct the pcf name in build script for ICESTICK

--- Basic/ICESTICK/Serial/makeit.sh
+++ Basic/ICESTICK/Serial/makeit.sh
@@ -1,7 +1,7 @@
 PROJECTNAME=serial
 VERILOGS="$PROJECTNAME.v uart.v"
 yosys -q -p "synth_ice40 -top $PROJECTNAME -json $PROJECTNAME.json" $VERILOGS || exit
-nextpnr-ice40 --json $PROJECTNAME.json --pcf ice40_pinout.pcf --asc $PROJECTNAME.asc --freq 12 --hx1k --package tq144 || exit
+nextpnr-ice40 --json $PROJECTNAME.json --pcf $PROJECTNAME.pcf --asc $PROJECTNAME.asc --freq 12 --hx1k --package tq144 || exit
 icepack $PROJECTNAME.asc $PROJECTNAME.bin || exit
 iceprog $PROJECTNAME.bin || exit
 echo DONE.
