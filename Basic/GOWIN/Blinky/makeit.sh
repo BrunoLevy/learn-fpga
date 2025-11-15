6546e17514bba7e8bce706ec8c1097bd8ba7ac82
Bruno <Bruno.Levy@inria.fr>
2025-11-15 06:56:42 +0100

    specify directory for FPGA tools so that we can compile different versions if need be.

--- Basic/GOWIN/Blinky/makeit.sh
+++ Basic/GOWIN/Blinky/makeit.sh
@@ -1,4 +1,5 @@
-yosys -D LEDS_NR=6 -DINV_BTN=0 -p "read_verilog blinky.v; synth_gowin -json blinky.json"
-nextpnr-gowin --json blinky.json --write pnrblinky.json --device GW1NR-LV9QN88PC6/I5 --family GW1N-9C --cst tangnano9k.cst
-gowin_pack -d GW1N-9C -o pack.fs pnrblinky.json
-/usr/bin/openFPGALoader -b tangnano9k pack.fs
+FPGA_ROOT=/usr/bin
+$FPGA_ROOT/yosys -D LEDS_NR=6 -DINV_BTN=0 -p "read_verilog blinky.v; synth_gowin -json blinky.json"
+$FPGA_ROOT/nextpnr-gowin --json blinky.json --write pnrblinky.json --device GW1NR-LV9QN88PC6/I5 --family GW1N-9C --cst tangnano9k.cst
+$FPGA_ROOT/gowin_pack -d GW1N-9C -o pack.fs pnrblinky.json
+$FPGA_ROOT/openFPGALoader -b tangnano9k pack.fs
